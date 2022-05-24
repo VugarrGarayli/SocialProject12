558266037f612178878e95f221032ed1dfc45784
gxra_uz6v <gxra_uz6v@STHQ0118-07>
2022-05-24 10:15:17 +0400

    Add project files.

---
+++ SocialProject.WebUI/Migrations/20220520100450_withPost.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialProject.WebUI.Migrations
+{
+    public partial class withPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CommentCount",
+                table: "Post",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VideoLink",
+                table: "Post",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentCount",
+                table: "Post");
+
+            migrationBuilder.DropColumn(
+                name: "VideoLink",
+                table: "Post");
+        }
+    }
+}
---
+++ SocialProject.WebUI/Migrations/20220520131244_withPosts.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialProject.WebUI.Migrations
+{
+    public partial class withPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_AspNetUsers_CustomIdentityUserId",
+                table: "Post");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Post",
+                table: "Post");
+
+            migrationBuilder.RenameTable(
+                name: "Post",
+                newName: "Posts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Post_CustomIdentityUserId",
+                table: "Posts",
+                newName: "IX_Posts_CustomIdentityUserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_CustomIdentityUserId",
+                table: "Posts",
+                column: "CustomIdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_CustomIdentityUserId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts");
+
+            migrationBuilder.RenameTable(
+                name: "Posts",
+                newName: "Post");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_CustomIdentityUserId",
+                table: "Post",
+                newName: "IX_Post_CustomIdentityUserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Post",
+                table: "Post",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_AspNetUsers_CustomIdentityUserId",
+                table: "Post",
+                column: "CustomIdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
